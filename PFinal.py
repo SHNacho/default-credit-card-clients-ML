15e69e398252742f3209a283764859e54ccdba82
Nacho Sánchez <nachosanchezh@hotmail.com>
2022-06-14 16:05:28 +0200

    Visualización y transformación de datos

--- PFinal.py
+++ PFinal.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV, KFold
 from sklearn.model_selection import learning_curve, ShuffleSplit
 from sklearn import preprocessing
 from sklearn.dummy import DummyClassifier
+from sklearn.neural_network import MLPClassifier
 
 import matplotlib.pyplot as plt
 import math
@@ -32,6 +33,14 @@ X = df.iloc[:, :23]
 
 print("Número de datos: ", X.shape[0])
 print("Número de atributos: ", X.shape[1])
+
+# Transformamos la variable educación de manera que se encuentre dividida
+# en 4 categorías como se indica en UCI y no en 6 como se encuntra realmente.
+# Esto se hace uniendo la clase minoritaria en una clase a la que llamaremos
+# "others"
+X['EDUCATION'] = X['EDUCATION'].where(X['EDUCATION'].isin([1, 2, 3]), 4)
+# Lo mismo con la variable de estado civil
+X['MARRIAGE'] = X['MARRIAGE'].where(X['MARRIAGE'].isin([1, 2]), 3)
 
 #%%
 
@@ -81,23 +90,172 @@ def hist_plot(serie, bins = None, rng = None, ylabel = 'Count'):
 # plt.xticks(ticks=[0, 1], labels=["Pago", "Impago"])
 # plt.show()
 
-bar_plot(y,
+bar_plot(y_train,
          xticks_labels=["Pago", "Impago"],
          title="Número de pagos frente a impagos")
 
-bar_plot(X['SEX'],
+bar_plot(X_train['SEX'],
          xticks_labels=['Male', 'Female'])
 
-bar_plot(X['EDUCATION'])
-        #  xticks_labels=['graduate school', 'university', 'high school', 'others'])
+bar_plot(X_train['EDUCATION'],
+         xticks_labels=['graduate school', 'university', 'high school', 'others'])
 
-bar_plot(X['MARRIAGE'])
-        #  xticks_labels=['Married', 'Single', 'Others'])
+bar_plot(X_train['MARRIAGE'],
+         xticks_labels=['Married', 'Single', 'Others'])
         
-hist_plot(X['AGE'])
+hist_plot(X_train['AGE'])
 
-# sn.countplot(x = X['SEX'])
-# plt.title("Género")
-# plt.xticks(ticks=[1, 2], labels=['Male', 'Female'])
-# plt.show()
 # %%
+
+def to_onehot(df, column):
+    '''
+    Transforma una columna del dataframe a one-hot encoding
+    Parameters
+    ----------
+    df : DataFrame
+        Dataframe.
+    column : String
+        Nombre de la columna a transformar.
+
+    Returns
+    -------
+    sub_df : DataFrame
+        Dataframe pasado con la codificación de la columna en formato
+        one-hot encoding.
+
+    '''
+    sub_df = df[column]
+    dum_df = pd.get_dummies(sub_df, prefix = column)
+    sub_df = df.join(dum_df)
+    sub_df = sub_df.drop(columns=[column])
+    return sub_df
+
+X_train = to_onehot(X_train, 'SEX')
+X_train = to_onehot(X_train, 'EDUCATION')
+X_train = to_onehot(X_train, 'MARRIAGE')
+
+X_test = to_onehot(X_test, 'SEX')
+X_test = to_onehot(X_test, 'EDUCATION')
+X_test = to_onehot(X_test, 'MARRIAGE')
+# X_train = to_onehot(X_train, 'PAY_0')
+# X_train = to_onehot(X_train, 'PAY_2')
+# X_train = to_onehot(X_train, 'PAY_3')
+# X_train = to_onehot(X_train, 'PAY_4')
+# X_train = to_onehot(X_train, 'PAY_5')
+# X_train = to_onehot(X_train, 'PAY_6')
+
+print("Nuevo número de variables: ", X_train.shape[1])
+
+
+
+# Escalamos las variables no binarias
+# Para ello primero separamos los datos en train y test
+# y los escalaremos todos en función de los de train
+def standard_scale_vars(train_data, test_data, variables_to_scale):
+    # Iniciamos el scalador
+    scaler = StandardScaler()
+    # Lo ajustamos a los datos de train
+    scaler.fit(train_data[variables_to_scale])
+    # Transformamos train y test con el escalador
+    train_data[variables_to_scale] = scaler.transform(train_data[variables_to_scale])
+    test_data[variables_to_scale] = scaler.transform(test_data[variables_to_scale])
+
+    
+    return train_data, test_data
+    
+# Variables a escalar. Las binarias no será necesario escalarlas
+variables_to_scale = ['LIMIT_BAL', 'AGE', 'PAY_0', 'PAY_2', 'PAY_3',
+                      'PAY_4', 'PAY_5', 'PAY_6', 'BILL_AMT1', 'BILL_AMT2',
+                      'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5', 'BILL_AMT6',
+                      'PAY_AMT1', 'PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4',
+                      'PAY_AMT5', 'PAY_AMT6']
+
+X_train, X_test = standard_scale_vars(X_train, X_test, variables_to_scale)
+
+#%%
+
+params = [
+            {
+                "penalty" : ['l1', 'l2'], # Tipo de regularización
+                "alpha" : [0.0001, 0.001, 0.01, 0.1], # Factor de regularización
+                "max_iter" : [10000],
+                "learning_rate": ['adaptive'], # Política de actualización del lr
+                "eta0" : [0.0001, 0.001, 0.01, 0.1], # Learning rate
+                # "l1_ratio" : [0.0, 0.30, 0.5, 0.7, 1.0] # Ratio entre L1 y L2
+            }
+]
+
+
+# Cross validator para pasarlo a GridSearchCV
+cv = KFold(n_splits=5, shuffle=False)
+
+best_estimators_rl = {}
+
+metrica = 'recall'
+print("------------ Regresión Logística ------------")
+# Creamos el estimador
+estimador = SGDClassifier(loss="log", 
+                          class_weight = "balanced",
+                          early_stopping=False, 
+                          n_jobs=-1,
+                          random_state=seed)        
+# Creamos el GridSearchCV
+clf = GridSearchCV(estimator = estimador,
+                   param_grid = params,
+                   cv = cv, 
+                   n_jobs = -1, scoring = metrica)
+# Lo entrenamos con los datos de train
+clf.fit(X_train, y_train)
+print("Mejor resultado: ", clf.best_score_)
+print("Mejores parámetros: ", clf.best_params_)
+print("")
+best_estimators_rl[metrica] = clf.best_estimator_
+
+y_pred = clf.predict(X_test)
+print("Test score: ", metrics.f1_score(y_test, y_pred))
+print("Test score: ", metrics.recall_score(y_test, y_pred))
+
+# Dibujamos la matriz de confusión
+def matriz_confusion(y_true, y_pred, title = None):
+    confusion_matrix = metrics.confusion_matrix(y_true, y_pred)
+    sn.heatmap(confusion_matrix, annot = True, fmt='d')
+    plt.title(title)
+    plt.xlabel('predicción')
+    plt.ylabel('valor real')
+    plt.show()
+
+matriz_confusion(y_test, y_pred)
+
+
+
+#%%
+
+# Multilayer Perceptron
+print("------------ Multilayer Perceptron ------------")
+
+
+clf = MLPClassifier(hidden_layer_sizes=(100,),
+                    activation='relu',
+                    solver='adam',
+                    learning_rate_init=0.0001,
+                    max_iter=1000,
+                    batch_size=64
+                    )
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_train)
+print("Train Accuracy: ", metrics.accuracy_score(y_train, y_pred))
+y_pred = clf.predict(X_test)
+print("Test Accuracy: ", metrics.accuracy_score(y_test, y_pred))
+print("Test f1: ", metrics.recall_score(y_test, y_pred))
+print("Test f1: ", metrics.recall_score(y_test, y_pred))
+
+#%%
+
+    
+matriz_confusion(y_test, y_pred)
+
+
+
+
+
+# print(X_train.describe().T)
